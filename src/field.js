b3ff7b2e74d8163d0649561dc71c166ae6f9918d
dongree <dongwoo0307@naver.com>
2021-01-23 20:03:39 +0900

    refactoring

---
+++ src/field.js
@@ -0,0 +1,68 @@
+'use strict'
+import * as sound from './sound.js';
+const carrotSound = new Audio('sound/carrot_pull.mp3');
+const CARROT_SIZE = 80;
+
+export const ItemType = Object.freeze({
+    carrot: 'carrot',
+    bug: 'bug',
+})
+
+export class Field {
+    constructor(carrotCount, bugCount){
+        this.carrotCount = carrotCount;
+        this.bugCount = bugCount;
+        this.field = document.querySelector('.game__field');
+        this.fieldRect = this.field.getBoundingClientRect();
+        this.field.addEventListener('click', this.onClick);
+    }
+
+    init() {
+        this.field.innerHTML = '';
+        this._addItem('carrot', this.carrotCount, 'img/carrot.png');
+        this._addItem('bug', this.bugCount, 'img/bug.png')
+    }
+
+setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+}
+
+    _addItem(className, count, imgPath) {
+        const x1 = 0;
+        const y1 = 0;
+        const x2 = this.fieldRect.width - CARROT_SIZE;
+        const y2 = this.fieldRect.height - CARROT_SIZE;
+    
+        for (let i=0; i < count; i++){
+            const item = document.createElement('img');
+            item.setAttribute('class', className);
+            item.setAttribute('src', imgPath);
+            item.style.position = 'absolute';
+    
+            const x = getRandomNumber(x1, x2);
+            const y = getRandomNumber(y1, y2);
+    
+            item.style.top = `${y}px`
+            item.style.left = `${x}px`
+    
+            this.field.appendChild(item);
+        }
+    }
+    
+
+    onClick= (event) => {
+        const target = event.target;
+        if(target.matches('.carrot')){
+            target.remove();
+            sound.playCarrot();
+            this.onItemClick && this.onItemClick(ItemType.carrot);
+        }
+        else if (target.matches('.bug')){
+            this.onItemClick && this.onItemClick(ItemType.bug);
+        }
+    }
+}
+
+function getRandomNumber(min, max){
+    return Math.random() * (max - min) + min;
+}
---
+++ src/main.js
@@ -0,0 +1,150 @@
+'use strict'
+import PopUp from './popup.js';
+import * as sound from './sound.js';
+import { GameBuilder, Reason }from './game.js';
+
+// Ellie's code
+
+const gameFinishBanner = new PopUp();
+const game = new GameBuilder()
+    .withGameDuration(10)
+    .withCarrotCount(10)
+    .withBugCount(10)
+    .build();
+
+game.setGameStopListener((reason) => {
+    let message;
+    switch(reason) {
+        case Reason.cancel:
+            message = 'Replay?';
+            sound.playAlert();
+            break;
+        case Reason.win:
+            message = 'You Won';
+            sound.playWin();
+            break;
+        case Reason.lose:
+            message = 'You Lost';
+            sound.playBug();
+            break;
+        default:
+            throw new Error('not valid reason');
+    }
+    gameFinishBanner.showWithText(message);
+})
+
+gameFinishBanner.setClickListener(()=> {
+    game.start();
+})
+
+// My code
+
+// const startBtn = document.querySelector('.startBtn');
+// const stopBtn = document.querySelector('.stopBtn');
+// const replayBtn = document.querySelector('.replayBtn');
+// const replayBox = document.querySelector('.replay');
+// const replayText = document.querySelector('.replayText');
+// const field = document.querySelector('.field');
+// const counter = document.querySelector('.counter');
+// const timer = document.querySelector('.timer');
+// let time = null;
+
+// const carrotsNum = 30;
+// const bugsNum = 20;
+// const timeLimit = 20;
+
+// // Sound
+// const carrotSound = new Audio('sound/carrot_pull.mp3');
+// const bugSound = new Audio('sound/bug_pull.mp3');
+// const bg = new Audio('sound/bg.mp3');
+// const game_win = new Audio('sound/game_win.mp3');
+// const alert = new Audio('sound/alert.wav');
+
+// function createRandomItems(num, name) {
+//     const fieldRight = field.getBoundingClientRect().right;
+//     const fieldBottom = field.getBoundingClientRect().bottom;
+//     const fieldLeft = field.getBoundingClientRect().left;
+//     const fieldTop = field.getBoundingClientRect().top;
+
+//     for (let i=0; i < num; i++){
+//         const item = document.createElement('img');
+//         item.setAttribute('class', `item ${name}`);
+//         item.setAttribute('src', `img/${name}.png`);
+        
+//         let randomX = getRandomNumber(fieldLeft, fieldRight);
+//         let randomY = getRandomNumber(fieldTop, fieldBottom);
+
+//         item.style.top = `${randomY}px`
+//         item.style.left = `${randomX}px`
+
+//         field.appendChild(item);
+//     }
+// }
+
+// function getRandomNumber(min, max){
+//     return Math.random() * (max - min) + min;
+// }
+
+// function showReplay(text) {
+//     replayBox.style.display = 'flex';
+//     replayText.textContent = text;
+//     clearInterval(time);
+// }
+
+// function timeout() {
+//     time = setInterval( () => {
+//         timer.innerHTML -= 1;
+//         if(timer.textContent == '0'){
+//             showReplay('Time Out!')
+//             alert.play();
+//             bg.pause();
+//         }
+//     }, 1000)
+// }
+
+// startBtn.addEventListener('click', () => {
+//     bg.play();
+//     createRandomItems(carrotsNum, 'carrot');
+//     createRandomItems(bugsNum, 'bug');
+//     startBtn.remove();
+//     stopBtn.style.display = 'inline';
+//     counter.innerHTML = carrotsNum;
+//     timer.innerHTML = timeLimit;
+//     timeout()
+// })
+
+// stopBtn.addEventListener('click', () => {
+//     showReplay('Replay?')
+//     alert.play();
+//     bg.pause();
+// })
+
+// replayBtn.addEventListener('click', () => {
+//     field.innerHTML = '';
+//     replayBox.style.display = 'none';
+//     createRandomItems(carrotsNum, 'carrot');
+//     createRandomItems(bugsNum, 'bug');
+//     counter.innerHTML = carrotsNum;
+//     timer.innerHTML = timeLimit;
+//     timeout();
+//     bg.play();
+// })
+
+// field.addEventListener('click', (e) => {
+//     const target = e.target;
+//     const name = target.className;
+//     if(name === 'item carrot'){
+//         carrotSound.play();
+//         target.remove();
+//         counter.innerHTML = counter.innerHTML-1;
+//         if(counter.textContent === '0'){
+//             showReplay('You Win');
+//             bg.pause();
+//             game_win.play();
+//         }
+//     } else if (name === 'item bug'){
+//         bugSound.play();
+//         bg.pause();
+//         showReplay('You lose')
+//     }
+// })
